3e02380d8dc224cc3dc866c4a8d071b84335590c
ZuoJiangtao <1799158837@qq.com>
2024-08-29 18:45:45 +0800

    feat: interface options

---
+++ types/options.d.ts
@@ -0,0 +1,13 @@
+interface Options {
+  // base
+  projectName?: string
+  overwrite?: boolean
+  framework?: string
+  useTypeScript?: boolean
+  useJsx?: boolean
+  useRouter?: boolean
+  usePinia?: boolean
+  useEslint?: boolean
+  usePrettier?: boolean
+  useVitest?: boolean
+}
