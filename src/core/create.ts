9f31aef50b8addc3aa486d2a333e10a5e7006592
ZuoJiangtao <1799158837@qq.com>
2024-09-02 11:48:46 +0800

    fix: fix overwrite del folder failed

--- src/core/create.ts
+++ src/core/create.ts
@@ -4,6 +4,17 @@ import { copyTemplate } from '@/utils/copyTemplate';
 import { ejsRender } from '@/utils/ejsRender';
 import { DEFAULT_PROJECT_NAME } from '@/config/compile.config';
 
+function recursiveDeleteFolder(filePath) {
+  for (const fileName of fs.readdirSync(filePath)) {
+    const fullPath = path.resolve(filePath, fileName);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      recursiveDeleteFolder(fullPath);
+      continue;
+    }
+    fs.unlinkSync(fullPath);
+  }
+}
+
 export default async function create(options: Options) {
   console.log(options);
   const {
@@ -21,38 +32,38 @@ export default async function create(options: Options) {
   } = options;
 
   const name = projectName || DEFAULT_PROJECT_NAME;
-  const fileNamePath = path.resolve(process.cwd(), name);
+  const filePath = path.resolve(process.cwd(), name);
 
   // ================= overwrite =================
-  if (fs.existsSync(fileNamePath) && shouldOverwrite) {
-    fs.writeFileSync(fileNamePath, '');
-  } else if (!fs.existsSync(fileNamePath)) {
-    fs.mkdirSync(fileNamePath);
+  if (fs.existsSync(filePath) && shouldOverwrite) {
+    recursiveDeleteFolder(filePath);
+  } else if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(filePath);
   }
 
   // ================= copy template =================
   // ========= base =========
-  await copyTemplate(path.resolve(__dirname, '../template/base'), fileNamePath);
+  await copyTemplate(path.resolve(__dirname, '../template/base'), filePath);
   // ========= config =========
   // eslint
   if (needsEslint) {
-    await copyTemplate(path.resolve(__dirname, '../template/config/eslint'), fileNamePath);
+    await copyTemplate(path.resolve(__dirname, '../template/config/eslint'), filePath);
   }
   // prettier
   if (needsPrettier) {
-    await copyTemplate(path.resolve(__dirname, '../template/config/prettier'), fileNamePath);
+    await copyTemplate(path.resolve(__dirname, '../template/config/prettier'), filePath);
   }
   // husky
   if (needsHusky) {
-    await copyTemplate(path.resolve(__dirname, '../template/config/husky'), fileNamePath);
+    await copyTemplate(path.resolve(__dirname, '../template/config/husky'), filePath);
   }
   // ========= framework =========
   // todo: 完整框架模板逻辑补充 - react\nuxt\next\koa
-  await copyTemplate(path.resolve(__dirname, `../template/${framework}`), fileNamePath);
+  await copyTemplate(path.resolve(__dirname, `../template/${framework}`), filePath);
 
   // ================= ejs render =================
   // ========= base =========
-  await ejsRender(path.resolve(__dirname, '../template/base'), fileNamePath, options);
+  await ejsRender(path.resolve(__dirname, '../template/base'), filePath, options);
   // ========= framework =========
-  await ejsRender(path.resolve(__dirname, `../template/${framework}`), fileNamePath, options);
+  await ejsRender(path.resolve(__dirname, `../template/${framework}`), filePath, options);
 }
