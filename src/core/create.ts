f8af0e736b1bf653f0804f825d28d0de1a7405b7
ZuoJiangtao <1799158837@qq.com>
2024-09-03 13:17:52 +0800

    chore: move vite plugin folder to template

--- src/core/create.ts
+++ src/core/create.ts
@@ -21,13 +21,8 @@ export default async function create(options: Options) {
     projectName,
     overwrite: shouldOverwrite,
     framework,
-    useTypeScript: needsTypeScript,
-    useJsx: needsJsx,
-    useRouter: needsRouter,
-    usePinia: needsPinia,
     useEslint: needsEslint,
     usePrettier: needsPrettier,
-    useVitest: needsVitest,
     useHusky: needsHusky,
   } = options;
 
@@ -57,6 +52,8 @@ export default async function create(options: Options) {
   if (needsHusky) {
     await copyTemplate(path.resolve(__dirname, '../template/config/husky'), filePath);
   }
+  // ========= vite =========
+  await copyTemplate(path.resolve(__dirname, '../template/vite'), filePath);
   // ========= framework =========
   // todo: 完整框架模板逻辑补充 - react\nuxt\next\koa
   await copyTemplate(path.resolve(__dirname, `../template/${framework}`), filePath);
@@ -64,6 +61,8 @@ export default async function create(options: Options) {
   // ================= ejs render =================
   // ========= base =========
   await ejsRender(path.resolve(__dirname, '../template/base'), filePath, options);
+  // ========= vite =========
+  await ejsRender(path.resolve(__dirname, '../template/vite'), filePath, options);
   // ========= framework =========
   await ejsRender(path.resolve(__dirname, `../template/${framework}`), filePath, options);
 }
