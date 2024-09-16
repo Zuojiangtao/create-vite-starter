8a750ecf8deef15b2b75db703db9a6d5ffcba89a
zuojt <1799158837@qq.com>
2024-09-16 15:45:02 +0800

    chore: utils type check and resolve

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,18 +4,23 @@
  * @param {Object} target the existing object
  * @param {Object} obj the new object
  */
-export function deepMerge(target, obj) {
-  const isObject = val => val && typeof val === 'object';
-  const mergeArrayWithDedupe = (a, b) => Array.from(new Set([...a, ...b]));
+export function deepMerge(target: object, obj: object) {
+  const isObject = (val: any) => val && typeof val === 'object';
+  const mergeArrayWithDedupe = (a: any[], b: any[]) => Array.from(new Set([...a, ...b]));
   for (const key of Object.keys(obj)) {
+    // @ts-ignore
     const oldVal = target[key];
+    // @ts-ignore
     const newVal = obj[key];
 
     if (Array.isArray(oldVal) && Array.isArray(newVal)) {
+      // @ts-ignore
       target[key] = mergeArrayWithDedupe(oldVal, newVal);
     } else if (isObject(oldVal) && isObject(newVal)) {
+      // @ts-ignore
       target[key] = deepMerge(oldVal, newVal);
     } else {
+      // @ts-ignore
       target[key] = newVal;
     }
   }
@@ -28,18 +33,20 @@ export function deepMerge(target, obj) {
  *
  * @param {any} packageJson - packageJson file stream
  * */
-export function sortDependencies(packageJson) {
+export function sortDependencies(packageJson: any) {
   const sorted = {};
 
   const depTypes = ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies'];
 
   for (const depType of depTypes) {
     if (packageJson[depType]) {
+      // @ts-ignore
       sorted[depType] = {};
 
       Object.keys(packageJson[depType])
         .sort()
         .forEach(name => {
+          // @ts-ignore
           sorted[depType][name] = packageJson[depType][name];
         });
     }
